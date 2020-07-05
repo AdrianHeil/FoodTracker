c87acc9bc3e982c1b4aab93dcafa287dc5738a90
Adrian <a.heil@outlook.com>
2020-07-06 00:16:03 +0200

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Adrian Heil on 06/07/20.
+//  Copyright © 2020 Adrian Heil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
